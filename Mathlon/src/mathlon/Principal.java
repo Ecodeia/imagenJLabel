7bfe0fc5b0fd027d036808cd316dfbc837ab5ad3
Armando <Armando@DESKTOP-JEF7N3N.cantv.net>
2023-11-05 02:16:42 -0400

    Clases Internas canal de YouTube @EcodeIA

---
+++ Mathlon/src/mathlon/Principal.java
@@ -0,0 +1,11 @@
+package mathlon;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mat mate= new Mat();
+		mate.setVisible(true);
+	}
+
+}
